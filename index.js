882a28e1014d57623af249f33b461af1b08a4c94
Dachi Darjania <dachidar19@gmail.com>
2026-06-25 18:06:34 +0400

    minor change

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express")
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.use(cookieParser())
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
   credentials: true
 }))
 app.use(express.json())
